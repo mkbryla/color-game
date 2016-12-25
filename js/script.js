04c712dc1e1101968dd5beef894dd07dc162845c
Michał Bryła <mkbryla@gmail.com>
2016-12-25 02:34:55 +0100

    HTML and CSS done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var rgbDisplay = document.querySelector("span");
+var text = document.getElementById("text");
+var newColorsBtn = document.getElementById("new-colors");
+var easyBtn = document.getElementById("easy");
+var hardBtn = document.getElementById("hard");
+
+var colors = [];
+var squares = [];
